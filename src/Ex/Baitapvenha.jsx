cd59b0dea601688b48952d00d99a6264fc4bf5d4
Humphrey Nguyen <humphreynguyen@Humphreys-Laptop.local>
2023-11-20 19:07:52 +0700

    firt

---
+++ src/Ex/Baitapvenha.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Background from "../../public/glassesImage/background.jpg"
+
+export default class Baitapvenha extends Component {
+  render() {
+    return (
+      <div style={{backgroundImage:`url(${Background})`}} >
+      </div>
+    )
+  }
+}
